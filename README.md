ccd839f4dd8ca85846e2aad2553a7b977fb8a4c7
GKNSB <manoshatzis@gmail.com>
2022-07-03 18:54:32 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# urlSeparator
-Identify non-wildcard unique URLs in Lepus findings
+## urlSeparator
+Identify non-wildcard unique URLs in Lepus findings. This is a simple helper script that assists in the identification of non-wildcard URLs. It parses either all or specific findings folders based on domain and separates the good urls from the shitty ones.
+
+```
+usage: urlSeparator.py [-h] [-d DOMAINS] lepusFindingsDir output
+
+I find urls in Lepus directories
+
+positional arguments:
+  lepusFindingsDir      Location of Lepus findings directory
+  output                Output file location
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -d DOMAINS, --domains DOMAINS
+                        Domain output folders to process separated by commas (Default 'all')
+```
---
+++ urlSeparator.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import os
+import re
+import shutil
+from dns.resolver import resolve
+from socket import gethostbyname
+from urllib.parse import urlparse
+from argparse import ArgumentParser, FileType
+
+parser = ArgumentParser(prog="urlSeparator.py", description="I find urls in Lepus directories")
+parser.add_argument("-d", "--domains", action="store", dest="domains", type=str, default="all", help="Domain output folders to process separated by commas (Default 'all')")
+parser.add_argument("lepusFindingsDir", help="Location of Lepus findings directory", type=str)
+parser.add_argument("output", help="Output file location", type=str)
+args = parser.parse_args()
+
+lepusFindingsDir = args.lepusFindingsDir
+output = args.output
+domainsToProcess = args.domains.split(",")
+
+try:
+    os.remove(output)
+except:
+    pass
+
+for folder in os.listdir(lepusFindingsDir):
+    cleanurls = []
+    urls = []
+
+    if folder in domainsToProcess or "all" in domainsToProcess:
+
+        print(f"[*] Processing {folder}")
+        folderFiles = os.listdir(os.path.join(lepusFindingsDir, folder))
+
+        if "urls.csv" in folderFiles:
+            with open(os.path.join(lepusFindingsDir, folder, "urls.csv"), "rb") as urlsFile:
+                for line in urlsFile.readlines():
+                    urls.append(line.decode("utf-8").strip())
+
+        if urls:
+            wildcards = []
+            if "wildcards.csv" in folderFiles:
+                with open(os.path.join(lepusFindingsDir, folder, "wildcards.csv"), "rb") as wildcardsFile:
+                    for line in wildcardsFile.readlines():
+                        wildcardName = line.decode("utf-8").split("|")[0]
+
+                        if wildcardName.startswith("."):
+                            wildcardName = wildcardName[1:]
+
+                        wildcardIP = line.decode("utf-8").split("|")[1]
+                        wildcards.append([wildcardName, wildcardIP])
+
+            if wildcards:
+                for wildcard in wildcards:
+                    for url in urls:
+                        hostname = urlparse(url).netloc.split(":")[0]
+
+                        ARecords = []
+                        with open(os.path.join(lepusFindingsDir, folder, "resolved_public.csv")) as resolutionsFile:
+                            for resline in resolutionsFile:
+                                if re.findall(f"{hostname}\|(.*)$", resline.strip()):
+                                    for IP in re.findall(f"{hostname}\|(.*)$", resline.strip())[0].split(","):
+                                        ARecords.append(IP)
+
+                        if not re.search(f"\.{wildcard[0]}", url) and not  wildcard[1] in ARecords:
+                            cleanurls.append(url)
+
+            else:
+                cleanurls = urls
+
+    if cleanurls:
+
+        with open(output, "a") as outfile:
+            for cleanurl in list(set(cleanurls)):
+                outfile.write(f"{cleanurl}\n")
